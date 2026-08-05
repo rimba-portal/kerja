75fe9e8c696bf7361ae050966a60c68b269f618c
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-05 12:48:43 +0800

    Release v1.2.7

--- src/WorkServiceProvider.php
+++ src/WorkServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Work;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class WorkServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
