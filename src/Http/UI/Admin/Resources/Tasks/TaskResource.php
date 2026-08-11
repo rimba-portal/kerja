9721bb9d6582bcc6d6b9b33e6dc21b0b5a2f3dff
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 10:17:35 +0800

    Release v1.2.9

--- src/Http/UI/Admin/Resources/Tasks/TaskResource.php
+++ src/Http/UI/Admin/Resources/Tasks/TaskResource.php
@@ -22,7 +22,7 @@ class TaskResource extends Resource
 {
     protected static ?string $model = Task::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ArrowSmallRight;
 
     protected static ?string $recordTitleAttribute = 'description';
 
--- src/Http/UI/Admin/Resources/WorkPackages/WorkPackageResource.php
+++ src/Http/UI/Admin/Resources/WorkPackages/WorkPackageResource.php
@@ -22,7 +22,7 @@ class WorkPackageResource extends Resource
 {
     protected static ?string $model = WorkPackage::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ArrowSmallRight;
 
     protected static ?string $recordTitleAttribute = 'name';
 
